9fefbe74adb0f0b166762cc88e9c3bcc0d45a2de
Lenovo <Lenovo@Simran26>
2025-09-03 21:40:19 +0530

    Initial commit - SeleniumProject

---
+++ src/test/java/Pages/HomePage.java
@@ -0,0 +1,21 @@
+package Pages;
+
+import static Test.SeleniumTest.driver;
+
+import org.openqa.selenium.By;
+
+public class HomePage {
+   public static String hamburger_menu_xpath ="//*[@id=\"menuToggle\"]/input";
+
+   public static String onlineproduct_link_xpath ="//*[@id=\"menu\"]/a[3]/li";
+   public static void click_hamburger_menu() {
+	 driver.findElement(By.xpath(hamburger_menu_xpath)).click();
+   }
+	public static void click_onlineproduct_link() throws InterruptedException {
+	Thread.sleep(2000);
+	driver.findElement(By.xpath(onlineproduct_link_xpath)).click();
+			
+
+	}
+
+}
