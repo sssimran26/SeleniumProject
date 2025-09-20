f4b4aa3505659c4ff7aec9813139bd6591319da7
sssimran26 <sssimransssharma48@gmail.com>
2025-09-20 09:22:10 +0530

    Fixed typos in XPaths, improved logging, and verified product page titles

--- src/test/java/Test/ProductPageTest.java
+++ src/test/java/Test/ProductPageTest.java
@@ -19,12 +19,12 @@ public class ProductPageTest {
 	public static String formalShoes_xpath="/html/body/div[2]/center/h2";
 	public static String sportsShoes_xpath="/html/body/div[3]/center/h2";
 	public static String sneakersShoes_xpath="/html/body/div[4]/center/h2";
-	public static String formalShoesdropdown_xapth="/html/body/div[2]/center/div";
-	public static String sportsShoesdropdown_xapth="/html/body/div[3]/center/div";
-	public static String SneakersShoesdropdown_xapth="/html/body/div[4]/center/div";
-	public static String formalShoesfirstname_xapth="/html/body/div[2]/table/tbody/tr[1]/td[1]";
-	public static String sportsShoesfirstname_xapth="/html/body/div[3]/table/tbody/tr[1]/td[1]";
-	public static String sneakersShoesfirstname_xapth="/html/body/div[4]/table/tbody/tr[1]/td[1]";
+	public static String formalShoesdropdown_xpath="/html/body/div[2]/center/div";
+	public static String sportsShoesdropdown_xpath="/html/body/div[3]/center/div";
+	public static String SneakersShoesdropdown_xpath="/html/body/div[4]/center/div";
+	public static String formalShoesfirstname_xpath="/html/body/div[2]/table/tbody/tr[1]/td[1]";
+	public static String sportsShoesfirstname_xpath="/html/body/div[3]/table/tbody/tr[1]/td[1]";
+	public static String sneakersShoesfirstname_xpath="/html/body/div[4]/table/tbody/tr[1]/td[1]";
     public static void formalShoes_verifyTitle() throws IOException {
     String expectedTitleFS="Formal Shoes";
     String actualTitleFS=driver.findElement(By.xpath(formalShoes_xpath)).getText();
@@ -75,8 +75,8 @@ public class ProductPageTest {
     
     public static void formalShoes_firstname_verify() throws IOException {
     	String expectedFirstFormalShoe="Classic Cheltenham";
-    	driver.findElement(By.xpath(formalShoesdropdown_xapth)).click();
-    	String actualFirstFormalShoe=driver.findElement(By.xpath(formalShoesfirstname_xapth)).getText();
+    	driver.findElement(By.xpath(formalShoesdropdown_xpath)).click();
+    	String actualFirstFormalShoe=driver.findElement(By.xpath(formalShoesfirstname_xpath)).getText();
 //    	Assert.assertEquals(expectedFirstFormalShoe.trim(), actualFirstFormalShoe.trim());
 //    	 if(expectedFirstFormalShoe.equals(actualFirstFormalShoe)) {
 //    		 WebElement element = driver.findElement(By.xpath(formalShoes_xpath));
@@ -96,8 +96,8 @@ public class ProductPageTest {
     
     public static void sportsShoes_firstname_verify() throws IOException {
     	String expectedFirstSportsShoe="Ultimate";
-    	driver.findElement(By.xpath(sportsShoesdropdown_xapth)).click();
-    	String actualFirstSportsShoe=driver.findElement(By.xpath(sportsShoesfirstname_xapth)).getText();
+    	driver.findElement(By.xpath(sportsShoesdropdown_xpath)).click();
+    	String actualFirstSportsShoe=driver.findElement(By.xpath(sportsShoesfirstname_xpath)).getText();
 //    	Assert.assertEquals(expectedFirstSportsShoe.trim(), actualFirstSportsShoe.trim());
 //    	 if(expectedFirstSportsShoe.equals(actualFirstSportsShoe)) {
 //    		 WebElement element = driver.findElement(By.xpath(sportsShoes_xpath));
@@ -117,8 +117,8 @@ public class ProductPageTest {
     
     public static void SneakersShoes_firstname_verify() throws IOException {
     	String expectedFirstSneakersShoe="Archivo";
-    	driver.findElement(By.xpath(SneakersShoesdropdown_xapth)).click();
-    	String actualFirstSneakersShoe=driver.findElement(By.xpath(sneakersShoesfirstname_xapth)).getText();
+    	driver.findElement(By.xpath(SneakersShoesdropdown_xpath)).click();
+    	String actualFirstSneakersShoe=driver.findElement(By.xpath(sneakersShoesfirstname_xpath)).getText();
 //    	Assert.assertEquals(expectedFirstSneakersShoe.trim(), actualFirstSneakersShoe.trim());
 //    	 if(expectedFirstSneakersShoe.equals(actualFirstSneakersShoe)) {
 //    		 WebElement element = driver.findElement(By.xpath(sneakersShoes_xpath));
